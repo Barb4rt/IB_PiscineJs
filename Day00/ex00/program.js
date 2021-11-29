cab580e1e3d7afb7783d595a763f276964658552
Barb4rt <ludwig.taeuber@gmail.com>
2021-11-29 11:03:57 +0100

    ex01 fait

---
+++ Day00/ex00/program.js
@@ -0,0 +1,22 @@
+function twoNumberSum(array, targetSum) {
+    if (array.length <= 0) {
+        console.log("Your array is empty, smart guy ;)");
+    }
+    let result = [];
+    for (let index = 0; index < array.length - 1; index++) {
+        const element1 = array[index];
+        for (let key = index + 1; key < array.length; key++) {
+            const element2 = array[key];
+            if (element1 + element2 == targetSum) {
+                result.push([element1, element2]);
+            }
+        }
+    }
+    if (!result.length > 0) {
+        console.log("No solution found... Sorry :(");
+    }
+    result.forEach((element) => {
+        console.log(element);
+    });
+}
+exports.twoNumberSum = twoNumberSum;
