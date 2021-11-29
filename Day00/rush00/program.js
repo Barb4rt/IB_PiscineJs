0bd256d9b832101e9599fed42b1437ea006ee33e
Barb4rt <ludwig.taeuber@gmail.com>
2021-11-29 20:55:34 +0100

    rush fait

--- Day00/rush00/program.js
+++ Day00/rush00/program.js
@@ -1,5 +1,6 @@
 function sun(buildings, direction){
     direction = direction.toLowerCase()
+    console.log(direction);
     if (direction !== "ouest" && direction !== "est" ) {
         return "Choisissez entre Est ou Ouest"
     }
@@ -9,6 +10,9 @@ function sun(buildings, direction){
     let result = []
     let maxHeight = 0
     for (let index = 0; index < buildings.length; index++) {
+        if(buildings[index] < 0 || !Number.isInteger(buildings[index])){
+            return "Le tableau contient des nombres négatifs ou décimals"
+        }
         if (buildings[index] > maxHeight) {
             maxHeight = buildings[index]
             result.push(index)
@@ -16,5 +20,4 @@ function sun(buildings, direction){
     }
     return result
 }
-
 exports.sun = sun ;
